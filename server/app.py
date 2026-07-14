ef0d0099afc6496dc71e9d62436860fbcbfcef66
Melanie Rodriguez <melarodrigg@gmail.com>
2026-07-13 23:55:34 -0600

    Added resource endpoints to app.py. Backend ready for testing

--- server/app.py
+++ server/app.py
@@ -2,12 +2,41 @@ from config import app, api
 from flask import request
 from flask_jwt_extended import verify_jwt_in_request, exceptions
 from models import User, Job, Contact, Event, Contact, Document
-from controllers import Signup, CheckJWTId, Login
+from controllers import Signup, CheckJWTId, Login, JobDashboard, Events, Contacts, Documents
 
 # Resources / Controllers
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckJWTId, '/checkjwtid', endpoint='checkjwtid')
 api.add_resource(Login, '/login', endpoint='login')
+
+api.add_resource(
+  JobDashboard, 
+  '/jobs', 
+  '/jobs/<int:job_id>', 
+  endpoint='jobs')
+
+api.add_resource(
+  Events, 
+  '/events', 
+  '/jobs/<int:job_id>/events',
+  '/jobs/<int:job_id>/events/<int:event_id>',
+  endpoint='events' )
+
+api.add_resource(
+  Contacts,
+  '/contacts',
+  '/jobs/<int:job_id>/contacts',
+  '/jobs/<int:job_id>/contacts/<int:contact_id>',
+  endpoint='contacts'
+)
+
+api.add_resource(
+  Documents,
+  '/documents',
+  '/jobs/<int:job_id>/documents',
+  '/jobs/<int:job_id>/documents/<int:document_id>',
+  endpoint='documents'
+)
 
 if __name__ == "__main__":
   app.run(debug=True, port=5555)
--- server/controllers/__init__.py
+++ server/controllers/__init__.py
@@ -1,4 +1,7 @@
 from .auth_controller import Signup, CheckJWTId, Login
 from .jobs_controller import JobDashboard
+from .events_controller import Events
+from .contacts_controller import Contacts
+from .documents_controller import Documents
 
-__all__ = ['Signup', 'CheckJWTId', 'Login', 'JobDashboard']
+__all__ = ['Signup', 'CheckJWTId', 'Login', 'JobDashboard', 'Events', 'Contacts', 'Documents']
